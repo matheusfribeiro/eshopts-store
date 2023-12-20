d228979473239cd16a196b7530fd54aacd0a0f9e
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-12-20 17:21:53 -0300

    product list and product card

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,26 @@
 import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
 import Billboard from "@/components/billboard"
 import Container from "@/components/ui/container"
+import ProductList from "@/components/product-list"
 
 export const revalidate = 0
 
 const HomePage = async () => {
   const billboard = await getBillboard("22c88e93-59fc-485b-a18a-347c33cb8f41")
+  const products = await getProducts({ isFeatured: true })
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produtos Destacados" items={products} />
+        </div>
       </div>
     </Container>
-  )
+  );
 }
 
 export default HomePage
