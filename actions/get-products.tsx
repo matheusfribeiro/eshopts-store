e22326287287d29142a1673db0a3f7243bb3c6ab
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-12-22 21:41:04 -0300

    feat: individual product screen

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -12,7 +12,7 @@ interface Query {
 }
 
 const getProducts = async (query: Query): Promise<Product[]> => {
-  const url = qs.stringify({
+  const url = qs.stringifyUrl({
     url: URL,
     query: {
       colorId: query.colorId,
@@ -21,7 +21,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
       isFeatured: query.isFeatured
     }
   })
-  const res = await fetch(URL)
+  const res = await fetch(url)
 
   return res.json()
 }
