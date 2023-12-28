24a00d3c312f2f9d4d4e68fb7b741dee6c0ad100
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-12-28 16:37:59 -0300

    feat: modal preview store

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from 'next/font/google'
 
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
 
 import './globals.css'
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
