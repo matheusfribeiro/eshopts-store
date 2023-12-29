74688d0abb468b6e3862ab94784f8e207dbcd547
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-12-29 18:42:26 -0300

    feat: add to cart func

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { ShoppingBag } from "lucide-react";
 import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation";
 
 import Button from "@/components/ui/button";
-import { ShoppingBag } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
 const [isMounted, setIsMounted] = useState(false)
@@ -12,6 +14,9 @@ useEffect(() => {
   setIsMounted(true);
 }, []);
 
+const router = useRouter()
+const cart = useCart()
+
 if (!isMounted) {
   return null;
 }
@@ -19,13 +24,13 @@ if (!isMounted) {
 
   return ( 
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push("/cart")} className="flex items center rounded-full bg-black px-4 py-2">
         <ShoppingBag 
           size={20}
           color="white"
         />
         <span className="ml-2 text-sm font-medium text-white">
-          0
+          {cart.items.length}
         </span>
       </Button>
     </div>
